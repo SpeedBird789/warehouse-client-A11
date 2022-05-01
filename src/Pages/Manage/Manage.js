9a5e05060bebc844173f32663ad34a9317dc65c8
SpeedBird789 <asifmahmoud787@gmail.com>
2022-05-01 05:10:55 +0100

    calling it a day

--- src/Pages/Manage/Manage.js
+++ src/Pages/Manage/Manage.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import useInventories from '../../hooks/useInventories';
 
 
 const Manage = () => {
-   
+   const [inventories, setInventories] = useInventories();
+
+   const handleDelete = id =>{
+       const proceed = window.confirm('Are you sure you want to delete this item?')
+       if(proceed){
+           const url = `http://localhost:5000/inventory/${id}`;
+           fetch(url, {
+               method: 'DELETE'
+           })
+           .then(res => res.json())
+           .then(data => {
+               console.log(data);
+               const remaining = inventories.filter(inventory => inventory._id !== id);
+               setInventories(remaining);
+           } )
+       }
+   }
 
     return (
-        <div>
+        <div className='w-50 mx-auto'>
             <h1 className='text-center mt-2'>Manage Your Inventory Here</h1>
 
+            {
+                inventories.map(inventory => <div key={inventory._id}>
+                    <h5>{inventory.name} <button onClick={()=> handleDelete(inventory._id)}>Delete</button> <button>Add New Item</button></h5>
+                </div>)
+            }
         </div>
     );
 };
